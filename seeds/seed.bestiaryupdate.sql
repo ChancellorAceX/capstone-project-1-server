6054c931844e621c5f41c502a1486a7e94c99c57
Adam Brown <adam.edward.brown@gmail.com>
2020-11-03 19:17:36 -0500

    Darkmantle

--- seeds/seed.bestiaryupdate.sql
+++ seeds/seed.bestiaryupdate.sql
@@ -593,7 +593,18 @@ VALUES
   null,null,null,null,null,
   'passive Perception 8','Giant',6,2300,
   ARRAY[['Poor Depth Perception','The cyclops has disadvantage on any attack roll against a target more than 30ft away']],
-  ARRAY[['Multiattack','The cyclops makes two greatclub attacks'],['Greatclub','Melee Weapon Attack: +9 to hit, reach 10ft, one target. Hit: (3d8 + 6) bludgeoning damage'],['Rock','Ranged Weapon Attack: +9 to hit, range 30/120ft, one target. Hit: (4d10 + 6) bludgeoning damage']]);
+  ARRAY[['Multiattack','The cyclops makes two greatclub attacks'],['Greatclub','Melee Weapon Attack: +9 to hit, reach 10ft, one target. Hit: (3d8 + 6) bludgeoning damage'],['Rock','Ranged Weapon Attack: +9 to hit, range 30/120ft, one target. Hit: (4d10 + 6) bludgeoning damage']]),
+  
+  ('Darkmantle','Small monstrosity, unaligned',
+  '11','5d6 + 5',
+  10,null,null,30,null,
+  16,3,12,1,13,1,2,-4,10,0,5,-3,
+  null,'Stealth +3',null,null,null,
+  'Blindsight 60ft, passive Perception 10',null,0.5,100,
+  ARRAY[['Echolocation','The darkmantle can''t use its blindsight while deafened'],['False Appearance','While the darkmantle remains motionless, it is indistinguishable from a cave formation such as a stalactite or stalagmite.']],
+  ARRAY[['Crush',E'Melee Weapon Attack: +5 to hit, reach 5ft, one creature. Hit: (1d6 + 3) bludgeoning damage, and the darkmantle attaches to the target. If the target is Medium or smaller and the darkmantle has advantage on the attack roll, it attaches by engulfing the target''s head, and the target is also blinded and unable to breathe while the darkmantle is attached in this way.\n  While attached to the target, the darkmantle can attack no other creature except the target but has advantage on its attack rolls. The darkmantle''s speed also becomes 0, it can''t benefit from any bonus to its speed, and it moves with the target.\n  A creature can detach the darkmantle by making a successful DC 13 Str check as an action. On its turn, the darkmantle can detach itself from the target by using 5ft of movement.'],['Darkness Aura (1/Day)','A 15ft radius of magical darkness extends out from the darkmantle, moves with it, and spreads around corners. The darkness lasts as long as the darkmantle maintains concentration, up to 10 minutes (as if concentrating on a spell). Darkvision can''t penetrate this darkness, and no natural light can illuminate it. If any of the darkness overlaps with an area of light created by a spell of 2nd level or lower, the spell creating the light is dispelled.']]),
+  
+  ();
   
   
   
