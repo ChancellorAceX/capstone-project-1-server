7935a4fefc5ed77a3965bbc14010f9a972296973
Adam Brown <adam.edward.brown@gmail.com>
2022-09-22 19:18:13 -0400

    troubleshoot fly 67

--- postgrator-config.js
+++ postgrator-config.js
@@ -3,8 +3,9 @@ require('dotenv').config();
 module.exports = {
   'migrationDirectory': 'migrations',
   'driver': 'pg',
-  'connectionString': process.env.NODE_ENV === 'test'
-    ? process.env.TEST_DATABASE_URL
-    : process.env.DATABASE_URL,
+  'connectionString':'postgres://postgres:887b0400eb581ed15259e54a2fd43f411f317e9f796ae5ab@battlesource-db.internal:5432/dnd5ebestiary',
+  // 'connectionString': process.env.NODE_ENV === 'test'
+  //   ? process.env.TEST_DATABASE_URL
+  //   : process.env.DATABASE_URL,
   'ssl': !!process.env.SSL
 };
